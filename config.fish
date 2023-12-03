3a19fbc35f296ae73a81230d1048753194991d35
John Zheng <bluevisor@gmail.com>
2023-12-03 05:43:06 +0000

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,8 @@
+function fish_greeting
+    neofetch
+    set -l line_count (wc -l < ~/greetings.txt)
+    set -l random_line (random choice 1 $line_count)
+    set -l quote (sed -n "$random_line"p ~/greetings.txt)
+    echo $quote
+    echo
+end
