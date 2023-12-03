4a3ef58f827e2ffc5244468685c03192e336d6cf
John Zheng <bluevisor@gmail.com>
2023-12-03 02:40:10 -0800

    Update config.fish
    bug fix

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 function fish_greeting
     neofetch
     set -l line_count (wc -l < ~/greetings.txt)
-    set -l random_line (random choice 1 $line_count)
+    set -l random_line (random 1 $line_count)
     set -l quote (sed -n "$random_line"p ~/greetings.txt)
     echo $quote
     echo
